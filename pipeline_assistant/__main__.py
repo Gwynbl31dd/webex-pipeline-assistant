49ce89b5f2102e9bda2ccdba51d0b2d45e79c949
Anthony Paulin <anthony.paulin@team.telstra.com>
2022-09-26 11:34:56 +1000

    Add html markdown

--- pipeline_assistant/__main__.py
+++ pipeline_assistant/__main__.py
@@ -86,8 +86,8 @@ def format(path,message):
         # create a table row
         row = [test_suite_name, test_suite_tests, test_suite_failures, test_suite_errors, test_suite_skipped, test_suite_time]
         table.append(row)
-        formated_table = tabulate(table, headers=headers,tablefmt="pretty")
-        message += '\n'+formated_table
+        formated_table = tabulate(table, headers=headers,tablefmt="html")
+        message += "\n"+"```html"+formated_table+"```"
     return message
 
 if __name__ == "__main__":  # pragma: no cover
